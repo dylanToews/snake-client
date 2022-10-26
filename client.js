e2c377c5998556b503dee70cb1a9cbf19768ec76
dylanToews <dylan.toews@yahoo.ca>
2022-10-26 12:20:09 -0600

    set up input and added a key to exit

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 const net = require("net");
 
-const name = "Name: DUC"
+const name = "Name: DUC";
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: "50541"
@@ -13,20 +13,20 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("data", (data) => {
-    console.log(data)
+    console.log(data);
 
-  })
+  });
 
   conn.on('connect', () => {
-    console.log("you iz connected")
-    conn.write(name)
+    console.log("you iz connected");
+    conn.write(name);
     //conn.write("Move: up")
-  })
+  });
   return conn;
 
 
 };
 module.exports = {
   net,
-  connect 
+  connect
 };
